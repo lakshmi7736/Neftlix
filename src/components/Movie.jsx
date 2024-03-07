ef5880d5f26c8aa97a428d361fbd23e82efdb3a1
CLINTPS <clintps1010@gmail.com>
2024-03-07 10:21:09 +0530

    First commit

---
+++ src/components/Movie.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import axios from "axios";
+import TrailerModal from "./TrailerModal";
+
+const Movie = ({ item }) => {
+  const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const { user } = UserAuth();
+  const [trailer, setTrailer] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const movieID = doc(db, "users", `${user?.email}`);
+
+  const saveShows = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Please log in to save a movie");
+    }
+  };
+
+  const handleMovie = async (id) => {
+    try {
+      const trailerResponse = await axios.get(
+        `http://api.themoviedb.org/3/movie/${id}/videos?api_key=de6aa365c3a8b8a53dc8204c32c1d18b&language=en-US`
+      );
+      setTrailer(trailerResponse.data.results[0]);
+      setIsModalOpen(!isModalOpen);
+    } catch (error) {
+      console.error("Error fetching trailer:", error);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div
+      onClick={() => handleMovie(item.id)}
+      className="w-[160px] sm:w-[200px] md:w-[240px] inline-block cursor-pointer relative p-2"
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+        alt={item?.title}
+      />
+      <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+        <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+          {item?.title}
+        </p>
+        <p onClick={saveShows}>
+          {like ? (
+            <FaHeart className="absolute top-4 left-4 text-gray-300" />
+          ) : (
+            <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+          )}
+        </p>
+      </div>
+      {isModalOpen && (
+        <TrailerModal
+          visible={isModalOpen}
+          trailer={trailer}
+          onClose={closeModal}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Movie;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {Link, useNavigate} from 'react-router-dom'
+import { UserAuth } from "../context/AuthContext";
+import logo from '../assets/Netflix_Logo_CMYK.png'
+
+const Navbar =()=> {
+  const {user, logOut} = UserAuth()
+  const navigate = useNavigate();
+  // console.log(user);
+  const HandleLogout = async ()=>{
+    try{
+      await logOut();
+      navigate('/')
+    }catch (error){
+      console.log(error);
+    }
+  }
+  return (
+    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
+      <Link to="/">
+        {/* <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1> */}
+        <img className="w-44" src={logo} alt="" />
+      </Link>
+      {user?.email ?(
+        <div>
+        <Link to="/account">
+          <button className="text-white pr-4">Account</button>
+        </Link>
+          <button onClick={HandleLogout}  className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white ">
+            Logout
+          </button>
+      </div>
+      ):(
+        <div>
+        <Link to="/login">
+          <button className="text-white pr-4">Sing In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white ">
+            Sing Up
+          </button>
+        </Link>
+      </div>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
